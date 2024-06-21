394ba5e4eeddfe9de432841f9fd0705afaaa4048
Kishorkishu95 <kishorkishu.b@outlook.com>
2024-06-21 19:30:24 +0530

    2nd project-task app modified

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -8,14 +8,11 @@ import {
   output,
 } from '@angular/core';
 import { type User } from './user.model';
-import { CardComponent } from "../shared/card/card.component";
 
 @Component({
     selector: 'app-user',
-    standalone: true,
     templateUrl: './user.component.html',
-    styleUrl: './user.component.css',
-    imports: [CardComponent]
+    styleUrl: './user.component.css'
 })
 export class UserComponent {
   @Input({ required: true })
